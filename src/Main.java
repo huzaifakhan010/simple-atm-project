dc8082a7f0ac0f32b1cf97f2c41280682e64d3b5
huzaifa <164413458+m5huzefa@users.noreply.github.com>
2024-05-19 21:50:38 +0500

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class atm{
+public class Main{
     public static void main(String[] args){
      Scanner sc = new Scanner(System.in);
       int balance = 10000, deposit,withdraw;
