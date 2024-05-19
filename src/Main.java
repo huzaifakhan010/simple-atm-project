bc3feb04af5ad5b012852d1cee55fc1b3904b28d
TishaMaheshwari <tishakumari19@gmail.com>
2024-05-19 23:38:38 +0500

    Third Commit(Isha noor)

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main{
 
       while(true){
            System.out.println( "choose 1 for deposit :");
-           System.out.println( "choose 2 for withdraw :");
+           System.out.println( "choose 2 for Withdraw :");
            System.out.println("choose 3 for balance inquiry :");
            System.out.println( "choose 4 for exit :");
            System.out.println( "choose correct operation to perform :");
