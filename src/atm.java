14d87bc2cbe64e2cfc6089b2b7bc7ce446c43bff
TishaMaheshwari <tishakumari19@gmail.com>
2024-05-19 16:45:35 +0500

    first commit ( Tisha)

--- src/atm.java
+++ src/atm.java
@@ -11,3 +11,14 @@ public class atm{
            System.out.println("choose 3 for balance inquiry :");
            System.out.println( "choose 4 for exit :");
            System.out.println( "choose correct operation to perform :");
+
+           int choice = sc.nextInt();
+           switch (choice) {
+            case 1:
+                  System.out.println( "Enter amount you want to deposit:");
+                  deposit = sc.nextInt();
+
+                  balance = balance + deposit;
+                   System.out.println("Your update balance is:"+balance);
+                   System.out.println();
+                break;
