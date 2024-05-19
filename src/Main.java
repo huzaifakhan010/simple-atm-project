f6a6d7b4dd4d1a8a8a5c3749d908fa4a55ba48e0
huzaifa <164413458+m5huzefa@users.noreply.github.com>
2024-05-19 13:41:33 +0500

    First commit(Huzaifa Khan)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
+import java.util.Scanner;
+
+public class atm{
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+      int balance = 10000, deposit,withdraw;
+
+      while(true){
+           System.out.println( "choose 1 for deposit :");
+           System.out.println( "choose 2 for withdraw :");
+           System.out.println("choose 3 for balance inquiry :");
+           System.out.println( "choose 4 for exit :");
+           System.out.println( "choose correct operation to perform :");
